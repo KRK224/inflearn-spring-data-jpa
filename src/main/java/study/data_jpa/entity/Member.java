831b85c426bf62b70b1b33ab881a06bd491b797f
krk224 <hihas93@naver.com>
2025-01-28 17:18:40 +0900

    [feat] JpaRepository 인터페이스를 활용한 공통 속성 + 쿼리 메소드.1 메소드 이름으로 쿼리 생성

---
+++ src/main/java/study/data_jpa/entity/Member.java
@@ -0,0 +1,55 @@
+package study.data_jpa.entity;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "fk_team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if( team!=null) {
+            changeTeam(team);
+        }
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
